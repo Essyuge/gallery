fa3f81475ed04ab2f27a0c3fae2f602eac098574
Essyuge <ugeesther@gmail.com>
2024-07-01 16:42:27 +0300

    Modified

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@cluster0.fgofhg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@cluster0.fgofhg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://estheruge:EFMCJrr13yM8uOMm@cluster0.fgofhg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+   
 }
 module.exports = config;
